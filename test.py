29a8369753f9e1cfaef47a0f78d7a4ec387899ad
Mang00o <soci.alex24@gmail.com>
2023-12-13 08:07:28 +0100

    correzione test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from example import sum
 from example import multiply
 
 def test_sum():
-    assert sum([2, 3]) == 5, "Should be 5"
+    assert sum(2, 3) == 5, "Should be 5"
 
 def test_multiply():
-    assert multiply([2,3]) == 6, "Should be 6"
+    assert multiply(2,3) == 6, "Should be 6"
