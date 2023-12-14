819254ab45d88ce64f0470c8f3503dabd537d03c
Mang00o <soci.alex24@gmail.com>
2023-12-14 11:48:04 +0100

    inserimento front end

---
+++ frontEnd/src/Header.js
@@ -0,0 +1,15 @@
+import './Header.css'
+
+function Header(){
+    return (
+      <div className='header'>
+        <ul>
+          <li>Home</li>
+          <li>Sto cazz</li>
+          <li>Boh?</li>
+        </ul>
+      </div>
+    );
+}
+
+export default Header;
