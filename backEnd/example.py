08b2890ad5f871926e3a4e61275ea45da1e196ef
Mang00o <soci.alex24@gmail.com>
2023-12-14 18:09:09 +0100

    inserimento file repo privata

--- backEnd/example.py
+++
@@ -1,7 +0,0 @@
-#file.py
-
-def sum(x, y):
-    return x + y
-
-def multiply(x, y):
-    return x * y
--- backEnd/tests.py
+++
@@ -1,8 +0,0 @@
-from example import sum
-from example import multiply
-
-def test_sum():
-    assert sum(2, 3) == 5, "Should be 5"
-
-def test_multiply():
-    assert multiply(2,3) == 6, "Should be 6"
